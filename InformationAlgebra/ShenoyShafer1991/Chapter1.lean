db49bf291ddfbc363924a8beee91ac0f420ec788
extradosages <extradosages.001@gmail.com>
2024-01-19 11:11:13 -0500

    chore: shenoy shafer cleanup

--- InformationAlgebra/ShenoyShafer1991/Chapter1.lean
+++ InformationAlgebra/ShenoyShafer1991/Chapter1.lean
@@ -1,6 +1,17 @@
 import Mathlib
 
 
+/-!
+# Shenoy, Shafer - 1991 - Local Computation in Hypertrees - Chapter 1
+
+This file establishes some basic definitions
+
+- `HyperGraph X`
+- `Branch ℋ b t` and `Twig ℋ t b`
+- `Hypertree ℋ`
+-/
+
+
 namespace HyperGraph
 
 
@@ -8,49 +19,67 @@ variable
   (X : Type*)
 
 
+/-- Elements of the underlying set of a hypergraph are called "vertices". -/
 abbrev Vertex := X
 
 
+/-- A hyperedge in a hypergraph is any set of vertices. -/
 abbrev HyperEdge := Set X
 
 
-abbrev HyperGraph := Finset (HyperEdge X)
+/-- A hypergraph consists of a (non-empty) family of subsets of a set. -/
+abbrev HyperGraph := Set (HyperEdge X)
 
 
-def isBranch
+protected def Intersecting {X} (b : HyperEdge X) (t : HyperEdge X) := b ∩ t ≠ ∅
+
+
+/-- In a hypergraph, one edge dominates another if the intersection of the latter with
+ANY other edge in the hypergraph is contained within the intersection of the former with that
+edge. -/
+protected def Dominating
     {X}
-    (hyperGraph : HyperGraph X)
+    (ℋ : HyperGraph X)
     (b : HyperEdge X)
     (t : HyperEdge X)
     :
     Prop
     :=
-  b ∩ t ≠ ∅ ∧ ∀ h ∈ hyperGraph, h ≠ t → ∀ x ∈ t, x ∈ h → x ∈ b
+  ∀ h ∈ ℋ, h ≠ t → ∀ x ∈ t, x ∈ h → x ∈ b
 
 
-def IsTwig
+/-- In a hypergraph, one edge is a branch relative to another if they intersect and if the
+former dominates the latter.
+
+See `HyperGraph.Dominating`.-/
+def Branch
     {X}
-    (hyperGraph : HyperGraph X)
+    (ℋ : HyperGraph X)
+    (b : HyperEdge X)
+    (t : HyperEdge X)
+    :
+    Prop
+    :=
+  HyperGraph.Intersecting b t ∧ HyperGraph.Dominating ℋ b t
+
+
+/-- In a hypergraph, the property of one edge being a twig relative to another is reciprocal to
+that of being a branch.
+
+See `Branch`.-/
+def Twig
+    {X}
+    (ℋ : HyperGraph X)
     (t : HyperEdge X)
     (b : HyperEdge X)
     :
     Prop
     :=
-  isBranch hyperGraph b t
+  Branch ℋ b t
 
 
-def Twig (hyperGraph : HyperGraph X) := {t : HyperEdge X | ∃ b : hyperGraph, IsTwig hyperGraph t b }
-
-
-example (l : List α) : Finset α := by
-  apply?
-  done
-
-
-/-- `Chain R a l` means that `R` holds between adjacent elements of `a::l`.
-```
-Chain R a [b, c, d] ↔ R a b ∧ R b c ∧ R c d
-``` -/
-inductive HyperTree : (t : HyperEdge X) → (H : Finset (HyperEdge X)) → (p : t ∉ H) → Prop
-  | nil {a : HyperEdge X} : HyperTree a ∅ _
-  | cons : ∀ {a b : HyperEdge X} {l : Finset (HyperEdge X)}, IsTwig l a b → HyperTree b l q → HyperTree a (Finset.cons b l q) p
+/-- `HyperTree t ℋ p` means that `t` is a twig for the hypertree `ℋ` and is not already a hyperedge
+in `ℋ` (via `p`).-/
+inductive HyperTree : (t : HyperEdge X) → (ℋ : HyperGraph X) → (p : t ∉ ℋ) → Prop
+  | nil {root : HyperEdge X} : HyperTree root ∅ _
+  | cons : ∀ {a b : HyperEdge X} {ℋ : HyperGraph X}, Twig ℋ a b → HyperTree b ℋ q → HyperTree a (Set.insert b ℋ) p
