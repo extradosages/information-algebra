f28670be493dff2c59fe199876ad2e93736218cf
extradosages <extradosages.001@gmail.com>
2024-01-22 17:28:35 -0500

    docs: add todos to top-level APIs

--- InformationAlgebra/ShaferShenoy1991/Basic.lean
+++ InformationAlgebra/ShaferShenoy1991/Basic.lean
@@ -1,5 +1,7 @@
 /-!
 Top-level API for this repository as a library is not yet implemented. Once a substantial part of
 the formalization has been completed, I'll start organizing tools.
+
+Shafer and Shenoy will probably be factored out.
 -/
 def hello := "world"
