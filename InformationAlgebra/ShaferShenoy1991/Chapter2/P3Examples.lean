d10549349f69df88f78d3f54332a81d7ec7fa50c
extradosages <extradosages.001@gmail.com>
2024-01-23 17:09:59 -0500

    feat: finish several examples

--- InformationAlgebra/ShaferShenoy1991/Chapter2/P3Examples.lean
+++ InformationAlgebra/ShaferShenoy1991/Chapter2/P3Examples.lean
@@ -1,5 +1,7 @@
 import InformationAlgebra.ShaferShenoy1991.Chapter2.P1HyperGraph
 import InformationAlgebra.ShaferShenoy1991.Chapter2.P2HyperTree
+import Mathlib.Tactic.FinCases
+import Mathlib
 
 open HyperGraph
 
@@ -27,21 +29,68 @@ def edge5 : HyperEdge Element := {W, Y, Z}
 
 def hyperGraph : HyperGraph Element := {edge1, edge2, edge3, edge4, edge5}
 
-theorem edge4_supports_edge1
-
-#check Finset.induction_on hyperGraph
-
-theorem edge1_branch_edge4 : Branch hyperGraph edge1 edge4 := by
-  constructor
-  · intro h
-    dsimp only [edge1, edge4] at h
-    contradiction
-  · constructor
-    · exact (bne_iff_ne (edge1 ∩ edge4) ∅).mp rfl
-    · whnf
-      intros c h_c_in h_c_ne x h_x_in
-      repeat cases h_c_in
-
+theorem supports_edge4_edge1 : HyperGraph.Supports hyperGraph edge4 edge1 := by
+  intros edge h_edge_in_hyperGraph h_edge_ne_edge4 vertex h_vertex_in_inter
+  dsimp only [Membership.mem] at h_edge_in_hyperGraph
+  dsimp only [HyperGraph.mem, HyperGraph.toFinset] at h_edge_in_hyperGraph
+  refine And.elim ?f (Finset.mem_inter.mp h_vertex_in_inter)
+  intros h_vertex_in₁ h_vertex_in₂
+  fin_cases h_vertex_in₁ <;> fin_cases h_edge_in_hyperGraph
+  all_goals repeat (first | contradiction | apply List.Mem.head | apply List.Mem.tail)
   done
 
-end Example1
+theorem edge4_ne_edge1 : edge4 ≠ edge1 := by
+  exact (bne_iff_ne edge4 edge1).mp rfl
+  done
+
+theorem branch_edge4_edge1 : Branch hyperGraph edge4 edge1 := by
+  constructor
+  · exact edge4_ne_edge1
+  · constructor
+    · exact (bne_iff_ne (edge4 ∩ edge1) ∅).mp rfl
+    · exact supports_edge4_edge1
+  done
+
+theorem supports_edge4_edge5 : HyperGraph.Supports hyperGraph edge4 edge5 := by
+  intros edge h_edge_in_hyperGraph h_edge_ne_edge4 vertex h_vertex_in_inter
+  dsimp only [Membership.mem] at h_edge_in_hyperGraph
+  dsimp only [HyperGraph.mem, HyperGraph.toFinset] at h_edge_in_hyperGraph
+  refine And.elim ?f (Finset.mem_inter.mp h_vertex_in_inter)
+  intros h_vertex_in₁ h_vertex_in₂
+  fin_cases h_vertex_in₁ <;> fin_cases h_edge_in_hyperGraph
+  all_goals repeat (first | contradiction | apply List.Mem.head | apply List.Mem.tail)
+  done
+
+theorem edge4_ne_edge5 : edge4 ≠ edge5 := by
+  exact (bne_iff_ne edge4 edge5).mp rfl
+  done
+
+theorem branch_edge5_edge4 : Branch hyperGraph edge4 edge5 := by
+  constructor
+  · exact edge4_ne_edge5
+  · constructor
+    · exact (bne_iff_ne (edge4 ∩ edge5) ∅).mp rfl
+    · exact supports_edge4_edge5
+  done
+
+theorem supports_edge3_edge5 : HyperGraph.Supports hyperGraph edge3 edge5 := by
+  intros edge h_edge_in_hyperGraph h_edge_ne_edge4 vertex h_vertex_in_inter
+  dsimp only [Membership.mem] at h_edge_in_hyperGraph
+  dsimp only [HyperGraph.mem, HyperGraph.toFinset] at h_edge_in_hyperGraph
+  refine And.elim ?f (Finset.mem_inter.mp h_vertex_in_inter)
+  intros h_vertex_in₁ h_vertex_in₂
+  fin_cases h_vertex_in₁ <;> fin_cases h_edge_in_hyperGraph
+  all_goals repeat (first | contradiction | apply List.Mem.head | apply List.Mem.tail)
+  done
+
+theorem edge3_ne_edge5 : edge3 ≠ edge5 := by
+  exact (bne_iff_ne edge3 edge5).mp rfl
+  done
+
+theorem branch_edge3_edge4 : Branch hyperGraph edge3 edge5 := by
+  constructor
+  · exact edge3_ne_edge5
+  · constructor
+    · exact (bne_iff_ne (edge3 ∩ edge5) ∅).mp rfl
+    · exact supports_edge3_edge5
+  done
