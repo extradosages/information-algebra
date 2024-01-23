7e162ebac3aa04517264cd61e8c6e0290e049e1b
extradosages <extradosages.001@gmail.com>
2024-01-23 17:11:56 -0500

    docs: update notice about public api

--- InformationAlgebra/Basic.lean
+++ InformationAlgebra/Basic.lean
@@ -1,5 +1,7 @@
 /-!
 Top-level API for this repository as a library is not yet implemented. Once a substantial part of
-the formalization has been completed, I'll start organizing tools.
+the formalization has been completed, I'll start organizing public API.
+
+For formalization progress, see sibling folders.
 -/
 def hello := "world"
--- InformationAlgebra/ShaferShenoy1991/Basic.lean
+++ InformationAlgebra/ShaferShenoy1991/Basic.lean
@@ -1,6 +1,8 @@
 /-!
 Top-level API for this repository as a library is not yet implemented. Once a substantial part of
-the formalization has been completed, I'll start organizing tools.
+the formalization has been completed, I'll start organizing the public API.
+
+For formalization progress, see sibling folders.
 
 Shafer and Shenoy will probably be factored out.
 -/
